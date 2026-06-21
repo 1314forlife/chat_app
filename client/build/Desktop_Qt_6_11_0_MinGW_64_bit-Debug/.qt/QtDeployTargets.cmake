ff34d7cf7128e802f4272fb64daf71620e566ace
liujian <905314863@qq.com>
2026-06-21 20:56:31 +0800

    移除 build 目录，更新 .gitignore

--- client/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_chat_client_FILE C:/my-github-project/rust/chat_app/client/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/chat_client.exe)
-set(__QT_DEPLOY_TARGET_chat_client_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_chat_client_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
